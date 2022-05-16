a4712f39c8b358afc228c08ff6596a571ca4c5ba
Lelton Borges <leltondev@gmail.com>
2022-05-16 11:54:13 -0300

    feat: Create project

---
+++ src/main/kotlin/org/dev/com/api/ApiForumAluraApplication.kt
@@ -0,0 +1,11 @@
+package org.dev.com.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiForumAluraApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiForumAluraApplication>(*args)
+}
---
+++ src/test/kotlin/org/dev/com/api/ApiForumAluraApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dev.com.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiForumAluraApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
