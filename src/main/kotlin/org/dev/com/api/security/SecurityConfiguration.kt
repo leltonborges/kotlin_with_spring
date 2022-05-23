02608236e599f0e21b5942076c3ae919d872a1e5
Lelton Borges <leltondev@gmail.com>
2022-05-22 23:14:57 -0300

    feat: add bean securityConfig

---
+++ src/main/kotlin/org/dev/com/api/security/SecurityConfiguration.kt
@@ -0,0 +1,34 @@
+package org.dev.com.api.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(
+    private val userDetailsService: UserDetailsService
+) : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        http?.cors()?.disable()
+        http?.authorizeRequests()?.anyRequest()?.authenticated()?.
+        and()?.
+        sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
+        and()?.formLogin()?.disable()?.httpBasic();
+    }
+
+    @Bean
+    fun bCrypetPasswordEncoder(): BCryptPasswordEncoder{
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth?.userDetailsService(userDetailsService)?.passwordEncoder(bCrypetPasswordEncoder())
+    }
+}
